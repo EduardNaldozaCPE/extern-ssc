5696c316a564a9464df25bfbb74908195daf3068
EduardNaldozaCPE <naldozaeduardpd@gmail.com>
2022-06-13 12:41:59 +0300

    styling: faq page adjust, select buttons rounded

--- js/pages/faq.js
+++ js/pages/faq.js
@@ -1,4 +1,10 @@
 function toggleFAQBox(item) {
+    if (item.getElementsByTagName('img')[0].style.transform == 'rotate(180deg)'){
+        item.getElementsByTagName('img')[0].style.transform = '';
+    } else {
+        item.getElementsByTagName('img')[0].style.transform = 'rotate(180deg)';
+    }
+
     let isdisplayed = (item.getElementsByClassName('faq-answer')[0].style.height == '0px');
     if (isdisplayed) {
         item.getElementsByClassName('faq-answer')[0].style.height = '250px';
@@ -18,6 +24,8 @@ function mobileUI() {
     logo.style.left = "50%"
     logo.style.transform = "translateX(-50%)"
     logo.style.width = "18%";
+    
+    document.getElementById('faq-filter').style.flexDirection = 'column';
 
     var content = document.getElementById('additrespanel');
     content.style.display = 'none';
@@ -45,5 +53,73 @@ class Page{
             `
         }
 
+    }
+}
+
+function highlight(text,inputText) {
+    // var inputText = document.getElementById("inputText");
+    var innerHTML = inputText.innerHTML;
+    var index = innerHTML.toLowerCase().indexOf(text.toLowerCase());
+    if (index >= 0) { 
+        innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
+        inputText.innerHTML = innerHTML;
+    }
+}  
+
+function clearhighlight() {
+    let highlighteds = document.getElementsByClassName('highlight');
+    for (let i = 0; i < highlighteds.length; i++) {
+        const highlighted = highlighteds[i];
+        let parent = highlighted.parentElement;
+        let t = parent.innerText;
+        highlighted.remove();
+        parent.innerText = t;
+    }
+}  
+
+function faqFilter() {
+    let s = document.getElementById('faq-filter-textID').value;
+    if (!(s == "" || s == " ")){
+        let qbox = document.getElementsByClassName('faq-box');
+        let filterlist = [];
+        clearhighlight();
+
+        for (let i_question = 0; i_question < qbox.length; i_question++) {
+            const box = qbox[i_question];
+            box.style.display = '';
+        }
+
+        for (let i_question = 0; i_question < qbox.length; i_question++) {
+            const box = qbox[i_question];
+
+            if (box.getElementsByTagName('h4')[0].innerText.toLowerCase().includes(s)){
+                highlight(s,box.getElementsByTagName('h4')[0]);
+                continue;
+            }
+
+            let qbox_answerlist = box.getElementsByClassName('faq-answer-li')
+            for (let i_answer = 0; i_answer < qbox_answerlist.length; i_answer++) {
+                const ans = qbox_answerlist[i_answer];
+
+                if (ans.innerText.toLowerCase().includes(s.toLowerCase())) {
+                    box.style.display = '';
+                    highlight(s,ans);
+                    break;
+                } else {
+                    box.style.display = 'none';
+                }
+                
+            }
+        }
+    }
+}
+
+function faqClear() {
+    let qbox = document.getElementsByClassName('faq-box');
+
+    clearhighlight()
+    for (let i_question = 0; i_question < qbox.length; i_question++) {
+        const box = qbox[i_question];
+        box.style.display = '';
     }
 }
